d3b7314e30230f976c2663a4ad7cc1e99d94ee97
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-11-27 22:09:37 +0000

    Update deprecation message in rd.sh

--- app/rd.sh
+++ app/rd.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 clear
-echo "This bit.ly link will stop working soon. The new install command will be;"
+echo "This bit.ly link is deprecated. The new install command is;"
 echo ""
 echo "curl -L install.batoaddons.app | bash"
 sleep 5
-echo ""
-echo "Now redirecting to continue instalation..."
-sleep 2
-clear
-curl -fsSL install.batoaddons.app | bash
+done
